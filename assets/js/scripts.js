39e60d3fa77e6c24329ead3e55856d672dd738a9
fplcampos <fplcampos@gmail.com>
2016-08-24 17:28:37 -0300

    Telas seção IMC

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,3 +10,16 @@ $("#topo").click(function() {
 $(".navbar-toggle").click(function() {
 	$(".navbar").slideToggle( "blind" );
 });
+
+jQuery( window ).scroll(function() {
+		
+	var st = jQuery(this).scrollTop(); // captura a posição do scroll
+	var formIMC = $(".formula-imc");
+
+	console.log(st);
+
+	if (st >= 450){
+		formIMC.delay().animate({'opacity': 1}, 1000);
+	};
+
+});
